c86a9fdcc260f1497cf7f21dd03946cd349ee7f7
Collin Dapper <collindapper@gmail.com>
2022-08-08 09:44:33 -0700

    heroku update

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'homepage#index'
-
-    get '/feeds' => 'feeds#index'
-
-    # Users
-    post '/sessions' => 'sessions#create'
-    get '/authenticated' => 'sessions#authenticated'
-    delete '/sessions' => 'sessions#destroy'
-
-    # Tweets
-    post '/tweets' => 'tweets#create'
-    get '/tweets' => 'tweets#index'
-    delete '/tweets/:id' => 'tweets#destroy'
-    get '/users/:username/tweets' => 'tweets#index_by_user'
-
-    # Redirect all other paths to index page, which will be taken over by AngularJS
-    # get '*path' => 'homepage#index'
+  
 end
